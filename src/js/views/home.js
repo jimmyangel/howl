ab1f94e57cd9a292fa647639e1bf95caddb124aa
Ricardo Morin <morinricardo@gmail.com>
2017-05-04 18:33:34 -0700

    Home view

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,8 +1,30 @@
 'use strict';
 //import {config} from '../config.js';
+import homeContent from '../../templates/home/homeContent.hbs';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
 
 export function setupView (viewer) {
   console.log('Home view...');
+  $('#homeContainer').html(homeContent());
+
+  imagesLoaded( '.grid', function() {
+    var masonry = new Masonry( '.grid', {
+      //initLayout: false,
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      transitionDuration: '.5s',
+      fitWidth: true,
+      gutter: 0
+    });
+    masonry.on('layoutComplete', function(items) {
+      $('.grid').css({'animation':'fadeIn ease-in 1', 'animation-duration':'.5s'});
+      $('.grid').css({'opacity':'1'});
+    });
+    masonry.layout();
+
+  });
+
   $('#homeContainer').show();
   $('.homeViewLinkItem').click(function() {
     console.log('goto', $(this).attr('view'));
