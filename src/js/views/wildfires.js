13c3873539e38a4509ed7ffe96bbcb9626919f27
Ricardo Morin <morinricardo@gmail.com>
2017-06-12 18:32:14 -0700

    Utils refactor

--- src/js/views/wildfires.js
+++ src/js/views/wildfires.js
@@ -57,7 +57,7 @@ export function setupView (viewer) {
   //data.getJSONData('data/MTBS/MTBSCZML.json', function(data) {
   data.getJSONData('data/MTBS/MTBSOregonFiresGen20170531Sampled.json', function(data) {
     fireListData = data;
-    var statsAndCZML = utils.makeCZMLAndStatsForListOfFires(fireListData);
+    var statsAndCZML = makeCZMLAndStatsForListOfFires(fireListData);
     statsAll = statsAndCZML.statsAll;
     setUpSummaryChart(statsAndCZML.stats, statsAll);
     Cesium.CzmlDataSource.load(statsAndCZML.czml).then(function(dataSource) {
@@ -101,6 +101,117 @@ export function setupView (viewer) {
     console.log(error);
     throw error;
   });
+}
+
+//TODO: remove hardcode of CZML interval
+function makeCZMLAndStatsForListOfFires (f) {
+  var mtbsCZML = [
+    {
+      id: 'document',
+      name: 'MTBS',
+      version: "1.0",
+      clock: {
+        interval: '1984-07-28T07:00:00.000Z/2015-12-31T23:59:59.999Z',
+        currentTime: '1984-07-28T07:00:00.000Z',
+        multiplier: 10000000,
+        range: 'LOOP_STOP',
+        step: 'SYSTEM_CLOCK_MULTIPLIER'
+      }
+    }
+  ];
+
+  var stats = {};
+  var statsAll = {numFires: 0, fromYear: Infinity, toYear: -Infinity};
+  var metricNames = [
+    'acres',
+    'severityHighAcres',
+    'severityModerateAcres',
+    'severityLowAcres',
+    'severityUnburnedAcres',
+    'severityIncreasedGreenesAcres',
+    'nonProcessingMaskAcres',
+    'forestAcres'
+  ];
+
+  f.features.forEach(function (feature) {
+    var year = (new Date(feature.properties.ignitionDate)).getUTCFullYear();
+    statsAll.fromYear = (statsAll.fromYear < year) ? statsAll.fromYear : year;
+    statsAll.toYear = (statsAll.toYear > year) ? statsAll.toYear : year;
+
+    metricNames.forEach(function(name) {
+      if (!(year in stats)) {
+        stats[year] = {numFires: 0};
+      }
+      if (!(name in stats[year])) {
+        stats[year][name] = 0;
+      }
+      if (!(name in statsAll)) {
+        statsAll[name] = 0;
+      }
+      stats[year][name] += feature.properties[name];
+      statsAll[name] += feature.properties[name];
+    });
+
+    statsAll.numFires++;
+    stats[year].numFires++;
+
+    var h, m, l;
+    /*var tot = feature.properties.severityHighAcres +
+                feature.properties.severityModerateAcres +
+                  feature.properties.severityLowAcres; */
+
+    var tot = feature.properties.acres;
+
+    var sev = [];
+    var colors = [
+      {r:255, g:0, b:0},
+      {r:255, g:255, b:0},
+      {r:121, g:255, b:211},
+    ]
+
+    h = feature.properties.severityHighAcres/tot;
+    m = feature.properties.severityModerateAcres/tot;
+    l = feature.properties.severityLowAcres/tot;
+    //u = feature.properties.severityUnburnedAcres/tot;
+
+    sev.push(h); sev.push(m); sev.push(l); //sev.push(u);
+
+    var idxMax = sev.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
+    var cylinderLength = 1000+feature.properties.severityHighAcres;
+    var czmlItem = {
+      id: feature.properties.id,
+      name: 'Fire Name: ' + feature.properties.name,
+      description:
+        'Fire Id: ' + feature.properties.id + '<br>' +
+        'Ignition Date: ' + feature.properties.ignitionDate + '<br>' +
+        'Acres: ' + feature.properties.acres + '<br> Severity Acres: <br>&nbspHigh: ' +
+          feature.properties.severityHighAcres + ' Moderate: ' +
+          feature.properties.severityModerateAcres + ' Low: ' +
+          feature.properties.severityLowAcres,
+      availability: year + '-01-01T00:00:00.000Z' + '/' + year + '-12-31T23:59:59.999Z',
+      cylinder: {
+        topRadius: 500+Math.sqrt(feature.properties.acres*4046),
+        bottomRadius:  500+Math.sqrt(feature.properties.acres*4046),
+        length: cylinderLength,
+        outline: false,
+        material : {
+          solidColor : {
+            color : {
+              rgba: [colors[idxMax].r, colors[idxMax].g, colors[idxMax].b, 200]
+            }
+          }
+        }
+      },
+      position: {
+        cartographicDegrees: [feature.geometry.coordinates[0], feature.geometry.coordinates[1], feature.geometry.coordinates[2] + cylinderLength/2]
+      }
+    };
+
+    mtbsCZML.push(czmlItem);
+  });
+
+  return {stats: stats, statsAll: statsAll, czml: mtbsCZML};
 }
 
 export function restoreView() {
