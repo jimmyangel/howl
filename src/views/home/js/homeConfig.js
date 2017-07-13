dc85312fe4bc9cd83c6f41c08a15f77b716118df
Ricardo Morin <morinricardo@gmail.com>
2017-07-12 20:10:59 -0700

    Change logo

--- src/views/home/js/homeConfig.js
+++ src/views/home/js/homeConfig.js
@@ -6,21 +6,21 @@ export var homeConfig = {
       name: 'wildfires',
       title: 'History of Wildfire Severity',
       summary: 'In the Pacific Northwest, fire is a natural part of healthy forests. Since 2004 the Federal Government has been conducting detailed wildfire severity assessments by analyzing before and after LANDSAT satellite images of fire boundaries. This \'spotlight\' shows the history of wildfires in Oregon and their severity since 1984.',
-      image: '../images/wildfires.jpg',
+      image: require('../images/wildfires.jpg'),
       imageCredit: 'Photo by the Oregon Department of Forestry'
     },
     {
       name: 'ecopwilderness',
       title: 'Potential Forest Wilderness',
       summary: 'Did you know that Oregon has the least amount of proportional protected wilderness area as compared to California and Washington? 4% versus 15% and 10% respectively! Using this \'spotlight\' you can explore potential forest wilderness areas that would raise Oregon to be on par with its neighbors.',
-      image: '../images/proposedwilderness.jpg',
+      image: require('../images/proposedwilderness.jpg'),
       imageCredit: 'Photo by Wikipedia user Axcordion'
     },
     {
       name: 'or7',
       title: 'The Journey of Wolf OR-7',
       summary: 'In 2011 a wolf by the name of OR-7, Journey, decided to break away from its pack in Northeast Oregon and march over 1,000 miles all the way to California through wilderness and roadless areas looking for a mate to establish his own pack. OR-7 had a GPS collar, so his route has been thoroughly recorded. This \'spotlight\' shows the incredible journey of OR-7.',
-      image: '../images/or7.jpg',
+      image: require('../images/or7.jpg'),
       imageCredit: 'Photo by the Oregon Department of Fish and Wildlife'
     }
   ]
