5889454a5d5b51df956aef0a27ef8596d928b636
Ricardo Morin <morinricardo@gmail.com>
2017-09-18 20:12:59 -0700

    Added update functionality

--- src/views/wthreats/js/wthreats.js
+++ src/views/wthreats/js/wthreats.js
@@ -12,18 +12,16 @@ import wthreatsListInfoPanel from '../templates/wthreatsListInfoPanel.hbs';
 import wthreatInfoBox from '../templates/wthreatInfoBox.hbs';
 import wthreatsChart from '../templates/wthreatsChart.hbs';
 
+import wthreatsUpdateModal from '../templates/wthreatsUpdateModal.hbs';
+
 import 'magnific-popup/dist/jquery.magnific-popup.min.js';
 import 'magnific-popup/dist/magnific-popup.css';
-
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
 
 import GitHub from 'github-api';
 // basic auth
 var gh = new GitHub({
-   username: 'jimmyangel',
-   password: '***REMOVED***'
+   username: 'NOUSER',
+   password: 'NOPASSWORD'
 });
 
 
@@ -49,9 +47,8 @@ var _viewer;
 var wthreatsDataSource;
 var statsAll;
 var viewerCallbacks = [];
+var wthreatsData;
 
-var firebaseDatabase = firebase.database();
-console.log(firebaseDatabase);
 //firebase.auth().signOut();
 /*firebase.database().ref('/wthreats').once('value').then(function(snapshot) {
   console.log(snapshot.val());
@@ -64,11 +61,25 @@ export function setupView (viewer) {
   _viewer = viewer;
 
   $('#cesiumContainer').on('contextmenu', function(e) {
-    console.log(firebase.auth().currentUser);
-    if (firebase.auth().currentUser) {
+    if (gh) {
       var pickItem = _viewer.scene.pick(new Cesium.Cartesian2(e.pageX, e.pageY));
       if (pickItem) {
-        console.log('right click', e.pageX, e.pageY, pickItem.id.properties.threatName.getValue());
+        var idx = wthreatsData.features.findIndex(function(f) {
+          return f.properties.threatName === pickItem.id.properties.threatName.getValue();
+        });
+        $.each(config.markerStyles, function(key, value){delete value.selected});
+        config.markerStyles[wthreatsData.features[idx].properties.threatType].selected = true;
+        $('#updateModal').html(wthreatsUpdateModal({threatsItem: wthreatsData.features[idx], threatSelect: config.markerStyles}));
+        $("form :input").change(function() {
+          console.log('form changed');
+        });
+        $('#commitButton').click(function() {
+          $('#updateModal').modal('hide');
+          commitDocument(idx);
+          return false;
+        });
+        $('#updateModal').modal('show');
+        console.log('right click', idx, e.pageX, e.pageY, pickItem.id.properties.threatName.getValue());
       }
     } else {
       console.log('Not logged on');
@@ -89,73 +100,133 @@ export function setupView (viewer) {
   //firebase.database().ref('/wthreats').once('value').then(function(snapshot) {
   data.getJSONData('https://raw.githubusercontent.com/oregonhowl/githubd/master/wthreats.json', function(data) {
     //var data = snapshot.val();
-    var tcount = 0;
-    data.features.forEach(function(feature) {
-      feature.properties['marker-color'] = config.markerStyles[feature.properties.threatType].color;
-      feature.properties['marker-symbol'] = config.markerStyles[feature.properties.threatType].icon;
-      if (statsAll[feature.properties.threatType]) {
-        statsAll[feature.properties.threatType]++;
-      } else {
-        statsAll[feature.properties.threatType] = 1;
-      }
-      tcount++;
-    });
-
-    $('#summaryChartContainer').html(wthreatsChart({tcount: tcount}));
-    setUpSummaryChart();
-
-    $('#infoPanel').html(wthreatsListInfoPanel({
-      markerStyles: config.markerStyles,
-      threats: data.features
-    }));
-
-    Cesium.GeoJsonDataSource.load(data).then(function(dataSource) {
-      wthreatsDataSource = dataSource;
-
-      wthreatsDataSource.entities.values.forEach(function(entity, idx) {
-        if (entity.billboard) {
-          entity.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM;
-          entity.billboard.heightReference = Cesium.HeightReference.CLAMP_TO_GROUND;
-        }
-        entity.ellipse = new Cesium.EllipseGraphics({
-          semiMajorAxis: 5000,
-          semiMinorAxis: 5000,
-          //distanceDisplayCondition: new Cesium.DistanceDisplayCondition(1000),
-          material: (Cesium.Color.fromCssColorString(config.markerStyles[data.features[idx].properties.threatType].color)).withAlpha(0.6)
-        });
-
-      });
-
-      _viewer.dataSources.add(wthreatsDataSource).then(function() {
-        window.spinner.stop();
-        viewdispatcher.cleanUrl();
-        utils.setUpResetView(_viewer);
-        $('#resetView').click();
-        setUpInfoBox();
-
-        $('#hide-circles-option').change(function() {
-          var hideCircles = $(this).is(":checked");
-          wthreatsDataSource.entities.values.forEach(function(entity) {
-            if (entity.ellipse) {
-              entity.ellipse.show = !hideCircles;
-            }
-          });
-        });
-
-        $('.v-legend-item-sel').click(function() {
-          var selected = $(this).text();
-          wthreatsDataSource.entities.values.forEach(function(entity) {
-            if (entity.properties.threatName.getValue() == selected) {
-              _viewer.selectedEntity = entity;
-              selectItem(entity);
-            }
-          });
-        });
-      });
-    });
+    wthreatsData = data;
+    refreshView();
 
   });
+}
 
+function refreshView() {
+  var tcount = 0;
+  wthreatsData.features.forEach(function(feature) {
+    feature.properties['marker-color'] = config.markerStyles[feature.properties.threatType].color;
+    feature.properties['marker-symbol'] = config.markerStyles[feature.properties.threatType].icon;
+    if (statsAll[feature.properties.threatType]) {
+      statsAll[feature.properties.threatType]++;
+    } else {
+      statsAll[feature.properties.threatType] = 1;
+    }
+    tcount++;
+  });
+
+  $('#summaryChartContainer').html(wthreatsChart({tcount: tcount}));
+  setUpSummaryChart();
+
+  $('#infoPanel').html(wthreatsListInfoPanel({
+    markerStyles: config.markerStyles,
+    threats: wthreatsData.features
+  }));
+
+  Cesium.GeoJsonDataSource.load(wthreatsData).then(function(dataSource) {
+    wthreatsDataSource = dataSource;
+
+    wthreatsDataSource.entities.values.forEach(function(entity, idx) {
+      if (entity.billboard) {
+        entity.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM;
+        entity.billboard.heightReference = Cesium.HeightReference.CLAMP_TO_GROUND;
+      }
+      entity.ellipse = new Cesium.EllipseGraphics({
+        semiMajorAxis: 5000,
+        semiMinorAxis: 5000,
+        //distanceDisplayCondition: new Cesium.DistanceDisplayCondition(1000),
+        material: (Cesium.Color.fromCssColorString(config.markerStyles[wthreatsData.features[idx].properties.threatType].color)).withAlpha(0.6)
+      });
+
+    });
+
+    _viewer.dataSources.add(wthreatsDataSource).then(function() {
+
+      window.spinner.stop();
+      viewdispatcher.cleanUrl();
+      utils.setUpResetView(_viewer);
+      $('#resetView').click();
+      setUpInfoBox();
+
+      $('#hide-circles-option').change(function() {
+        var hideCircles = $(this).is(":checked");
+        wthreatsDataSource.entities.values.forEach(function(entity) {
+          if (entity.ellipse) {
+            entity.ellipse.show = !hideCircles;
+          }
+        });
+      });
+
+      $('.v-legend-item-sel').click(function() {
+        var selected = $(this).text();
+        wthreatsDataSource.entities.values.forEach(function(entity) {
+          if (entity.properties.threatName.getValue() == selected) {
+            _viewer.selectedEntity = entity;
+            selectItem(entity);
+          }
+        });
+      });
+
+      $('.v-legend-item-sel').on('contextmenu', function() {
+        console.log('hey', $(this));
+        document.getSelection().removeAllRanges();
+        updateThreatInfoDialog($(this).text());
+        return false;
+      });
+
+    });
+  });
+}
+
+function updateThreatInfoDialog (selected) {
+  if (gh) {
+    var idx = wthreatsData.features.findIndex(function(f) {
+      return f.properties.threatName === selected;
+    });
+    $.each(config.markerStyles, function(key, value){delete value.selected});
+    config.markerStyles[wthreatsData.features[idx].properties.threatType].selected = true;
+    $('#updateModal').html(wthreatsUpdateModal({threatsItem: wthreatsData.features[idx], threatSelect: config.markerStyles}));
+    $("form :input").change(function() {
+      console.log('form changed');
+    });
+    $('#commitButton').click(function() {
+      $('#updateModal').modal('hide');
+      commitDocument(idx);
+      return false;
+    });
+    $('#updateModal').modal('show');
+  } else {
+    console.log('Not logged on');
+  }
+}
+
+function commitDocument(idx) {
+  console.log(idx);
+  wthreatsData.features[idx].properties.threatName = $('#threat-name').val();
+  wthreatsData.features[idx].properties.threatType = $('#threat-type').val();
+  wthreatsData.features[idx].properties.threatDescription = $('#threat-description').val();
+  wthreatsData.features[idx].geometry.coordinates[0] = $('#threat-lon').val();
+  wthreatsData.features[idx].geometry.coordinates[1] = $('#threat-lat').val();
+  wthreatsData.features[idx].properties.threatImgUrl = $('#threat-img-url').val();
+  wthreatsData.features[idx].properties.threatImgCredit = $('#threat-img-credit').val();
+  wthreatsData.features[idx].properties.threatUrlReferences = [];
+  if ($('#threat-info-url-1').val()) {
+    wthreatsData.features[idx].properties.threatUrlReferences.push({url: $('#threat-info-url-1').val(), urlTitle: $('#threat-info-url-title-1').val()});
+  }
+  if ($('#threat-info-url-2').val()) {
+    wthreatsData.features[idx].properties.threatUrlReferences.push({url: $('#threat-info-url-2').val(), urlTitle: $('#threat-info-url-title-2').val()});
+  }
+
+  repo.writeFile('master', 'wthreats.json', JSON.stringify(wthreatsData, null, 2), 'Update ' + wthreatsData.features[idx].properties.threatName, {encode: true}).then(function() {
+    wipeoutView();
+    refreshView();
+  }, function(error) {
+    console.log('commit error', error);
+  });
 }
 
 function selectItem(e) {
