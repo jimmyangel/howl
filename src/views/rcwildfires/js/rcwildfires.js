c1e0627d47f1e6e1e15e187bb578e965af4bf518
Ricardo Morin <morinricardo@gmail.com>
2018-04-01 17:18:30 -0700

    Incremental update of rcwildfires spotlight

--- src/views/rcwildfires/js/rcwildfires.js
+++ src/views/rcwildfires/js/rcwildfires.js
@@ -9,8 +9,8 @@ import * as data from '../../../js/data.js';
 import * as utils from '../../../js/utils.js';
 import * as user from '../../../js/user.js';
 
-import wthreatsListInfoPanel from '../templates/rcwildfiresListInfoPanel.hbs';
-import wthreatsChart from '../templates/rcwildfiresChart.hbs';
+import rcwildfiresListInfoPanel from '../templates/rcwildfiresListInfoPanel.hbs';
+import rcwildfiresChart from '../templates/rcwildfiresChart.hbs';
 
 import 'magnific-popup/dist/jquery.magnific-popup.min.js';
 import 'magnific-popup/dist/magnific-popup.css';
@@ -41,31 +41,32 @@ export function setupView (viewer) {
   data.getJSONData(config.dataPaths.rcwildfiresList, function(data) {
     rcwildfireListData = data;
 
-    makeCZMLAndStatsForListOfRcfires(rcwildfireListData, function(c) {
-      Cesium.CzmlDataSource.load(c).then(function(dataSource) {
-        rcwildfireListDataSource = dataSource;
+    $('#infoPanel').html(rcwildfiresListInfoPanel({
+      listOfFires: rcwildfireListData
+    }));
 
-        _viewer.dataSources.add(dataSource).then(function() {
+    Cesium.CzmlDataSource.load(makeCZMLAndStatsForListOfRcfires(rcwildfireListData)).then(function(dataSource) {
+      rcwildfireListDataSource = dataSource;
 
-          console.log('epa', rcwildfireListDataSource);
+      _viewer.dataSources.add(dataSource).then(function() {
 
-          window.spinner.stop();
-          viewdispatcher.popUpLinkClickHandler = function(id) {
-            var eId = rcwildfireListDataSource.entities.getById(id).properties.eId.getValue();
-            //this.inViewDispatch(gotoArea.bind(this, eId) , '?view=ecopwilderness&eId=' + eId);
-          }
+        console.log('epa', rcwildfireListDataSource);
 
-          utils.setUpResetView(_viewer);
-          $('#resetView').click();
+        window.spinner.stop();
+        viewdispatcher.popUpLinkClickHandler = function(id) {
+          this.inViewDispatch(gotoFire.bind(this, id) , '?view=rcwildfires&fId=' + id);
+        }
 
-          viewdispatcher.cleanUrl();
-        });
+        utils.setUpResetView(_viewer);
+        $('#resetView').click();
+
+        viewdispatcher.cleanUrl();
       });
     });
   });
 }
 
-function makeCZMLAndStatsForListOfRcfires (rcwildfireListData, callback) {
+function makeCZMLAndStatsForListOfRcfires (rcwildfireListData) {
   var rcwildfiresCZML = [
     {
       id: 'document',
@@ -73,32 +74,30 @@ function makeCZMLAndStatsForListOfRcfires (rcwildfireListData, callback) {
       version: "1.0",
     }
   ];
-  Cesium.when(pinBuilder.fromMakiIconId(config.markerStyles.fire.icon, Cesium.Color.fromCssColorString(config.markerStyles.fire.color), 30),function(canvas) {
-    rcwildfireListData.forEach(function (f) {
-      var pathToFlameIcon = require('../../../images/flame.png');
-      var czmlItem = {
-        id: f.fireFileName,
-        name: f.fireName,
-        billboard: {
-          image : pathToFlameIcon,
-          verticalOrigin: 'BOTTOM',
-          heightReference: 'CLAMP_TO_GROUND',
-          scale: 0.05,
-          scaleByDistance: {
-            nearFarScalar: [2e5, 2, 1.8e6, 0.1]
-          }
-        },
-        position: {
-          cartographicDegrees: [f.location[0], f.location[1], 1000]
-        },
-        properties: {
-          howlHasFeaturePopUp: true
+  rcwildfireListData.forEach(function (f) {
+    var pathToFlameIcon = require('../../../images/flame.png');
+    var czmlItem = {
+      id: f.fireFileName,
+      name: f.fireName,
+      billboard: {
+        image : pathToFlameIcon,
+        verticalOrigin: 'BOTTOM',
+        heightReference: 'CLAMP_TO_GROUND',
+        //scale: 0.05,
+        scaleByDistance: {
+          nearFarScalar: [2e4, 0.1, 1.8e6, 0.005]
         }
-      };
-      rcwildfiresCZML.push(czmlItem);
-    });
-    return callback(rcwildfiresCZML);
+      },
+      position: {
+        cartographicDegrees: [f.location[0], f.location[1], 1000]
+      },
+      properties: {
+        howlHasFeaturePopUp: true
+      }
+    };
+    rcwildfiresCZML.push(czmlItem);
   });
+  return rcwildfiresCZML;
 }
 
 function selectItem(e) {
@@ -107,6 +106,14 @@ function selectItem(e) {
 
 export function restoreView() {
 
+}
+
+function gotoAll() {
+
+}
+
+function gotoFire(id) {
+  console.log('go to', id);
 }
 
 export function wipeoutView() {
