6182c45d605f33e1e76ec37faeed482647c86ee3
Ricardo Morin <morinricardo@gmail.com>
2018-08-11 17:06:08 -0700

    Update MTBS data source

--- src/views/wildfires/js/wildfiresConfig.js
+++ src/views/wildfires/js/wildfiresConfig.js
@@ -4,7 +4,7 @@ import {defaultDataPathBaseUrl} from '../../../js/config.js';
 
 export var config = {
   dataPaths: {
-    wildfiresList: defaultDataPathBaseUrl + '/MTBS/MTBSOregonFiresGen20170531Sampled.json',
+    wildfiresList: defaultDataPathBaseUrl + '/MTBS/MTBSOregonFiresGen20180811Sampled.json',
     wildfiresFireKmz: defaultDataPathBaseUrl + '/MTBS/kmz/'
   }
 }
