f2615c9da2bd2f83a68f9f20cdee54319ee8daf7
Ricardo Morin <morinricardo@gmail.com>
2020-01-22 13:29:00 -0800

    Cleanup config

--- src/views/wildfires/js/wildfiresConfig.js
+++ src/views/wildfires/js/wildfiresConfig.js
@@ -4,7 +4,7 @@ import {defaultDataPathBaseUrl} from '../../../js/config.js';
 
 export var config = {
   dataPaths: {
-    wildfiresList: defaultDataPathBaseUrl + '/MTBS/MTBSSampled.json',
+    wildfiresList: defaultDataPathBaseUrl + '/MTBS/MTBS.json',
     wildfiresFireKmz: defaultDataPathBaseUrl + '/MTBS/kmz/'
   }
 }
